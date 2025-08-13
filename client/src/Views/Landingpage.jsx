ccfab3526c45728542b100d13a9e6e87c0f7a486
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-13 14:30:39 +0530

    add breakpoint md

--- client/src/Views/Landingpage.jsx
+++ client/src/Views/Landingpage.jsx
@@ -20,18 +20,18 @@ function Landingpage() {
             <div className='flex flex-col w-screen max-h-max bg-[#ffd6a5]  '>
                 <Navbar />
                 <div className='flex-1 grid place-items-center '>
-                    <div className='flex justify-center items-center rounded m-5  bg-[#FFFFFF] w-5/6 max-h-max   '>
+                    <div className='flex justify-center items-center rounded m-5 bg-[#FFFFFF] w-5/6 max-h-max sm:w-3/4 sm:px-5 '>
                         <div className='p-3'>
 
                             {/* First div */}
-                            <div className='p-3  my-5 w-full  border-2  '>
-                                <div className='text-[#FBA943] text-4xl font-bold uppercase mx-5 tracking-wide '>Donor<br />Circle</div>
-                                <div className='mx-5 my-4 tracking-wide border-2'>
+                            <div className='p-3 my-5 w-full  border-2 sm:py-7 md:px-4  '>
+                                <div className='text-[#FBA943] text-4xl font-bold uppercase mx-5 tracking-wide sm:w-full sm:mx-1 md:mx-1 md:text-4xl border-2'>Donor<br />Circle</div>
+                                <div className='mx-5 my-4 tracking-wide sm:w-full sm:mx-1  border-2'>
                                     Accidents, Surgeries, Cancer treatments,
                                     All need blood. Every single day.
-                                    Be someone’s miracle.<br/> <span className='text-[#FBA943] text-2xl font-bold opacity-0 animate-fadeIn mt-5 '  >Join DonorCircle.</span>
+                                    Be someone’s miracle.<br/> <span className='text-[#FBA943] text-2xl font-bold opacity-0 animate-fadeIn mt-5  '  >Join DonorCircle.</span>
                                 </div>
-                                <Button name={"SignUp"} className={" ml-5 bg-[#FBA943] text-white px-6 py-3 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg hover:shadow-blue-400/50 "}
+                                <Button name={"SignUp"} className={"ml-5 bg-[#FBA943] text-white px-6 py-3 rounded-lg transition-all duration-300 hover:scale-105 hover:shadow-lg hover:shadow-blue-400/50 sm:py-2 sm:mx-1 sm:w-full "}
                                     onClick={() => {
                                         navigate("/signup")
                                     }}
