cc6fe632a2a3a3a26dcd4fdf84c575146d14c2b5
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-13 16:00:24 +0530

    add breakpoint 2xl

--- client/src/Views/Landingpage.jsx
+++ client/src/Views/Landingpage.jsx
@@ -17,16 +17,16 @@ function Landingpage() {
 
     return (
         <>
-            <div className='flex flex-col w-screen  bg-[#ffd6a5]  xl:h-screen border-8 '>
+            <div className='flex flex-col w-screen h-screen bg-[#ffd6a5]  xl:h-screen  '>
                 <Navbar />
                 <div className='flex-1 grid place-items-center '>
-                    <div className='flex justify-center items-center rounded m-5 bg-[#FFFFFF] w-5/6 max-h-max sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5     border-4  '>
-                        <div className='p-3 lg:flex lg:h-4/5 lg:my-7 border-4'>
+                    <div className='flex justify-center items-center rounded m-5 bg-[#FFFFFF] w-5/6 max-h-max sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 2xl:w-3/5  '>
+                        <div className='p-3 lg:flex lg:h-4/5 lg:my-7 '>
 
                             {/* First div */}
-                            <div className='p-3 my-5 w-full  border-2 sm:py-7 md:px-4 lg:w-4/6 xl:w-5/6 xl:h-5/6 '>
-                                <div className='text-[#FBA943] text-4xl font-bold uppercase mx-5 tracking-wide sm:w-full sm:mx-1 md:mx-1 md:text-4xl lg:mt-10 border-2'>Donor<br />Circle</div>
-                                <div className='mx-5 my-4 tracking-wide sm:w-full sm:mx-1  lg:text-base  border-2'>
+                            <div className='p-3 my-5 w-full sm:py-7 md:px-4 lg:w-4/6 xl:w-5/6 xl:h-5/6 '>
+                                <div className='text-[#FBA943] text-4xl font-bold uppercase mx-5 tracking-wide sm:w-full sm:mx-1 md:mx-1 md:text-4xl lg:mt-10 '>Donor<br />Circle</div>
+                                <div className='mx-5 my-4 tracking-wide sm:w-full sm:mx-1  lg:text-base 2xl:w-3/4  '>
                                     Accidents, Surgeries, Cancer treatments,
                                     All need blood. Every single day.
                                     Be someone’s miracle.<br/> <span className='text-[#FBA943] text-2xl font-bold opacity-0 animate-fadeIn mt-5  '  >Join DonorCircle.</span>
@@ -39,8 +39,8 @@ function Landingpage() {
                             </div>
 
                             {/* second div */}
-                            <div className='p-3 my-5 w-full  grid place-items-center border-2   '>
-                                <Image img_url={img} className="w-auto h-3/6 -mt-7 sm:object-scale-down md:object-scale-down lg:object-cover   border-2" />
+                            <div className='p-3 my-5 w-full  grid place-items-center    '>
+                                <Image img_url={img} className="w-auto h-3/6 -mt-7 sm:object-scale-down md:object-scale-down lg:object-cover   " />
                             </div>
                         </div>
                     </div>
