9160e1b81dbc1d5208e72ca00346a19437e984e0
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-13 09:06:11 +0530

    add file

---
+++ client/src/Views/Singup.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//Components Import
+import SingupForm from '../Components/singupForm'
+
+function Adddetails() {
+    return (
+        <>
+            <div className='w-screen h-screen bg-[#ffd6a5] '>
+                {/* <h1>Singup Page</h1> */}
+                <SingupForm />
+            </div>
+
+        </>
+    )
+}
+
+export default Adddetails
