247b60a58f5bd49d222993ea11ee14a7392cdc0c
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-13 09:05:09 +0530

    Add SingupForm

---
+++ client/src/Components/SingupForm.jsx
@@ -0,0 +1,238 @@
+import imgurl from '../assets/longin_image/signup_rem_bg.png'
+import { useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast';
+import { useForm } from "react-hook-form"
+import { DevTool } from '@hookform/devtools'
+
+//lucid icons
+import { User, Mail, LockKeyhole, Key, BadgeCheck } from 'lucide-react';
+
+
+import axios from 'axios'
+// import dotenv from 'dotenv'
+
+//Components Import
+import Label from './Label'
+import InputField from './InputField.jsx'
+import Image from './Image.jsx'
+import Button from './Button.jsx'
+import LinkTo from './LinkTo.jsx'
+
+
+
+
+
+
+function SingupForm() {
+
+    //react-hook-form
+
+    const { register, handleSubmit, formState: { errors }, control, watch, reset } = useForm({
+        mode: "onChange",
+        defaultValues: {
+            fullname: "",
+            username: "",
+            email: "",
+            password: "",
+            // confirm_pass: ""
+        }
+    });
+
+    // watches live changes
+    const fullname = watch("fullname");
+    const username = watch("username_field");
+    const email = watch("email_field");
+    const password = watch("pass_field");
+
+    // console.log("uuuu=",fullname)
+
+    const AddFormData = async (e) => {
+       
+        e.preventDefault();
+        try {
+            const response = await axios.post(`http://localhost:5001/Users/signup`, {
+                name: fullname,
+                username: username,
+                email: email,
+                password: password,
+                // confirm_pass: confirm_pass
+            });
+            console.log(response?.data)
+
+            toast.success(response?.data?.message);
+            reset();
+            // toast.dismiss();
+        }
+        catch (error) {
+            // toast.dismiss();
+            toast.error(error?.response?.data?.message)
+        }
+    }
+
+    const onSubmit = (data) => {
+        console.log("Form Submitted...", data)
+    }
+
+    return (
+        <>
+            <div className='flex flex-col justify-center items-center w-screen h-screen bg-[#ffd6a5] '>
+                {/* <Navbar /> */}
+                <div className='flex justify-center items-center rounded bg-white w-3/5 h-4/5 p-4 '>
+
+                    {/* second Div */}
+                    <div className=''>
+                        <p className='text-2xl font-semibold ml-2 tracking-wider '>Sign Up</p>
+                        <Image img_url={imgurl} className="w-auto " />
+                    </div>
+
+                    {/* First Div */}
+                    <div className=' overflow-y-auto overflow-x-hidden scrollbar-hide  h-fit'>
+                        <form className=' flex flex-col w-auto  h-[450px] '
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
+                            
+
+                            <div className='p-1 relative  '>
+                                <Label title="Name"  />
+                                <InputField
+                                    type="text"
+                                    placeholder="John Doe"
+                                    name="fullname"
+                                    {...register("fullname", {
+                                        required: "Name is required",
+                                        pattern: {
+                                            value: /^(?:[A-Z][a-z]*)(?:\s[A-Z][a-z]*)*$/,
+                                            message: "Capitalize each word,letters/spaces only",
+                                        },
+                                        maxLength: {
+                                            value: 30,
+                                            message: "Max 30 chars",
+                                        },
+                                    })}
+
+
+                                    icon={<User />} />
+                                {
+                                    errors?.fullname && <span className='text-red-500 text-x w-full  absolute left-2  -bottom-5 '>{errors?.fullname?.message}</span>
+                                }
+                            </div>
+
+                            <div className=' p-1  mt-5 relative'>
+                                <Label title="Username" />
+                                <InputField
+                                    type="text"
+                                    placeholder="john_doe"
+                                    name="username_field"
+
+                                    {...register("username_field", {
+                                        required: "Username required",
+                                        pattern: {
+                                            value: /^[a-z0-9_]{3,16}$/,
+                                            message: "3–15 chars, start with letter, letters/numbers/_ only",
+                                        },
+                                    })}
+
+                                    icon={<BadgeCheck />} />
+
+                                {
+                                    errors?.username_field && <span className='text-red-500 text-x w-full  absolute left-2  -bottom-10 '>{errors?.username_field?.message}</span>
+                                }
+                            </div>
+
+                            <div className=' p-1  mt-5 relative'>
+                                <Label title="Email" />
+                                <InputField
+                                    type="email"
+                                    name="email_field"
+                                    placeholder="user@example.com"
+                                    {...register("email_field", {
+                                        required: "Email is required",
+                                        pattern: {
+                                            value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/,
+                                            message: "Enter a valid email address",
+                                        },
+                                    })}
+
+                                    icon={<Mail />} />
+                                {
+                                    errors?.email_field && <span className='text-red-500 text-x w-full  absolute left-2 -bottom-5 '>{errors?.email_field?.message}</span>
+                                }
+                            </div>
+
+                            <div className=' p-1  mt-5 relative'>
+                                <Label title="Password" />
+                                <InputField
+                                    type="password"
+                                    // value={formdata?.password}
+                                    name="pass_field"
+                                    {...register("pass_field", {
+                                        required: "Password is required",
+
+                                        pattern: {
+                                            value: /^(?=.*[!@#$%^&*(),.?":{}|<>])[0-9!@#$%^&*(),.?":{}|<>]{4,6}$/,
+                                            message: "Password must be 4–6 characters, digits and at least one special character",
+                                        },
+                                    })}
+                                    // onChange={(e) => {
+                                    //     setFormdata({
+                                    //         ...formdata,
+                                    //         password: e.target.value
+                                    //     })
+                                    // }}
+                                    icon={<LockKeyhole />}
+                                />
+                                {
+                                    errors?.pass_field && <span className='text-red-500 text-x w-full  absolute left-2 -bottom-10 '>{errors?.pass_field?.message}</span>
+                                }
+                            </div>
+
+                            {/* <div className=' p-1  mt-5 relative'>
+                                <Label title="Confirm Password" />
+                                <InputField
+                                    type="password"
+                                    name="conf_pass_field"
+                                    value={formdata?.confirm_pass}
+                                    {...register("conf_pass_field", {
+                                        required: "Confirm Password is required",
+                                                            //need to add new validations
+                                        // pattern: {
+                                        //     value: /^(?=.*[!@#$%^&*(),.?":{}|<>])[0-9!@#$%^&*(),.?":{}|<>]{4,6}$/,
+                                        //     message: "Password must be 4–6 characters, digits and at least one special character",
+                                        // },
+                                    })}
+                                    onChange={(e) => {
+                                        setFormdata({
+                                            ...formdata,
+                                            confirm_pass: e.target.value
+                                        })
+                                    }}
+                                    icon={<Key />}
+                                />
+                                {
+                                    errors?.conf_pass_field && <span className='text-red-500 text-x w-full  absolute  -bottom-5 '>{errors?.conf_pass_field?.message}</span>
+                                }
+                            </div> */}
+
+                            <div className=' p-1  mt-10 mb-5'>
+                                <Button name="Create Account"
+                                    onClick={AddFormData}
+                                    // onClick={handleSubmit(onSubmit)}
+                                    className="!px-6 !py-2 !rounded-lg !w-full"
+                                />
+                            </div>
+
+                            <div className='p-2 flex justify-center items-center  mt-1 '>
+                                <LinkTo url={"/signin"} className="capitalize font-normal !text-black text-center w-full " >All ready have an account ? <br /> <span className='text-blue-800 hover:text-blue-600 '>Sign in</span></LinkTo>
+                            </div>
+                        </form>
+                        <DevTool control={control} />
+                    </div>
+                </div>
+
+                <Toaster />
+            </div>
+        </>
+    )
+}
+
+export default SingupForm
