e8cafbc0313a16e548964fce9360940ba49d276f
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-14 20:46:08 +0530

    add height

--- client/src/Views/Landingpage.jsx
+++ client/src/Views/Landingpage.jsx
@@ -17,7 +17,7 @@ function Landingpage() {
 
     return (
         <>
-            <div className='flex flex-col w-screen h-screen bg-[#ffd6a5]  xl:h-screen  '>
+            <div className='flex flex-col w-screen min-h-screen bg-[#ffd6a5]  xl:h-screen  '>
                 <Navbar />
                 <div className='flex-1 grid place-items-center '>
                     <div className='flex justify-center items-center rounded m-5 bg-[#FFFFFF] w-5/6 max-h-max sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 2xl:w-3/5  '>
