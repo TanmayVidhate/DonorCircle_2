78caa569f170d8deeb2fe3530fb1aa5817d3294b
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-19 14:25:03 +0530

    add navbar and div

--- client/src/Views/ContactUs.jsx
+++ client/src/Views/ContactUs.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+// import Component
+import Navbar from '../Components/Navbar'
+
 function ContactUs() {
     return (
         <div className='flex flex-col w-screen min-h-screen bg-[#ffd6a5]  xl:h-screen  '>
-            <h1>ContactUs</h1>
-            <p>Not yet done.</p>
+            <Navbar />
+            <div className='flex-1 grid place-items-center '>
+                <div className='flex justify-center  items-center border rounded-lg m-5 mt-24 bg-[#FFFFFF] w-5/6   sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 2xl:w-3/5  '>
+                    <h1>ContactUs</h1>
+                    <p>Not yet done.</p>
+                </div>
+            </div>
         </div>
     )
 }
