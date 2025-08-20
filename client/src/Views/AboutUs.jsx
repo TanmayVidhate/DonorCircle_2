e9838863f88794c62f9bc8f4889e6aa16454530a
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-20 11:05:37 +0530

    add div and m

--- client/src/Views/AboutUs.jsx
+++ client/src/Views/AboutUs.jsx
@@ -7,17 +7,19 @@ function AboutUs() {
     return (
         <div className='flex flex-col w-screen min-h-screen bg-[#ffd6a5]  xl:h-screen  '>
             <Navbar />
-            <div className='flex-1 grid place-items-center  '>
-                <div className='border rounded-lg m-5 mt-24 bg-[#FFFFFF] w-5/6  p-5  sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 2xl:w-3/5 '>
+            <div className='flex-1 grid place-items-center'>
+                <div className='border rounded-lg m-5 mt-24 bg-[#FFFFFF] w-5/6  p-5  sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 lg:h-4/6 2xl:w-3/5 '>
                     <p className=' text-2xl font-semibold  tracking-wider  m-5 '>About Us</p>
 
-                    <div className="text-gray-700 leading-relaxed  m-5">Welcome to <span className='font-semibold text-red-500'>DonorCircle</span> a platform created to connect blood donors with those in need. We believe that a single donation can save multiple lives, and our mission is to make the
-                        process of finding and contacting donors simple, fast, and reliable.
+                    <div className='lg:my-16 '>
+                        <div className="text-gray-700 leading-relaxed  m-5">Welcome to <span className='font-semibold text-red-500'>DonorCircle</span> a platform created to connect blood donors with those in need. We believe that a single donation can save multiple lives, and our mission is to make the
+                            process of finding and contacting donors simple, fast, and reliable.
+                        </div>
+                        <div className="text-gray-700 leading-relaxed  m-5">At DonorCircle, donors can easily register their details, and people in need can directly
+                            reach out to them without any middleman. We aim to build a community of responsible donors and create a network where help is always just a click away.
+                        </div>
+                        <div className="text-center text-red-600 font-semibold mt-2">Together, we can make a difference and give the gift of life.</div>
                     </div>
-                    <div className="text-gray-700 leading-relaxed  m-5">At DonorCircle, donors can easily register their details, and people in need can directly
-                        reach out to them without any middleman. We aim to build a community of responsible donors and create a network where help is always just a click away.
-                    </div>
-                    <div className="text-center text-red-600 font-semibold mt-2">Together, we can make a difference and give the gift of life.</div>
                 </div>
             </div>
         </div>
