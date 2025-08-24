1edfd94c3ec8e9f45ae72213b4617519cee7a2ee
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-08-24 10:15:43 +0530

    add register

--- client/src/Components/UploadFile.jsx
+++ client/src/Components/UploadFile.jsx
@@ -4,14 +4,15 @@ import React from 'react'
 //Components import
 import Label from './Label'
 
-function UploadFile({ type, name, onChange }) {
+function UploadFile({ type, name, onChange,...register }) {
     // console.log("type=",type);
     // console.log("name=",name);
-    // console.log("onChange=",onchange);
+    // console.log("register=",...register);
     return (
         <>
             <div className=''>
                 <input className=' m-1 p-2 rounded w-96 focus:outline-none ' 
+                {...register}
                 type={type}
                 name={name} 
                 onChange={onChange} />
