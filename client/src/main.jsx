f2898a488f4b98b8a66c9bb7eb01b1c90d8bb363
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-18 11:45:24 +0530

    add routes file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,34 +7,12 @@ import App from './App.jsx'
 
 import '@fontsource/balsamiq-sans'; 
 
-//Views Import
-import Landingpage from "./Views/Landingpage.jsx"
-import Signin from "./Views/Signin.jsx"
-import ShowAllUser from "./Views/ShowAllUser.jsx"
-import AddUserProfile from "./Views/AddUserProfile.jsx"
-import ShowAllUserinfo from "./Views/ShowAllUserInfo.jsx"
-import AboutUs from './Views/AboutUs.jsx';
-import Contact from './Views/ContactUs.jsx';
-import FAQ from './Views/FAQ.jsx';
-
-//Components Import
-import SingupForm from "./Components/SingupForm.jsx"
-// import Cards from "./Components/Cards.jsx"
+import AppRoutes from './routes/AppRoutes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/signup" element={<SingupForm />} />
-        <Route path="/signin" element={<Signin/>}/>
-        <Route path="/showalluers" element={<ShowAllUser/>}/>
-        <Route path="/showuserinfo/:email" element={<ShowAllUserinfo/>}/>
-        <Route path="/adduserprofile" element={<AddUserProfile />} />
-        <Route path='/aboutus' element={<AboutUs/>} />
-        <Route path='/contactus' element={<Contact/>} />
-        <Route path='/faq' element={<FAQ/>}/>
-      </Routes>
+      <AppRoutes/>
     </BrowserRouter>
   </StrictMode>,
 )
