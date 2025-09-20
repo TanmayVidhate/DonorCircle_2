0236568a6321dd787087fc4850a26ba1cd7302a5
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-20 09:47:21 +0530

    add className

--- client/src/Components/ContactUSCard.jsx
+++ client/src/Components/ContactUSCard.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import Button from './Button'
-function ContactUSCard({title,info,mobile,action,btntext}) {
+function ContactUSCard({img,title,info,mobile,action,btntext}) {
     return (
         <>
-            <div className='border rounded-md text-center w-2/6  h-max m-2 py-10 '>
-                <div className='border m-auto border-red-500 rounded-full w-1/2 h-2/5 '>
-                    <p className='text-center'>Img</p>
+            <div className='border rounded-md text-center w-2/6 h-[300px] m-2 p-2 '>
+                <div className='border m-auto border-red-500 rounded-full w-[30%] h-[25%] mt-3'>
+                    <p className=''>{img}</p>
                 </div>
-                <div>
+                <div className='mt-3'>
                     <p>{title}</p>
                 </div>
-                <div>
-                    <p className='text-gray-400'>{info}</p>
+                <div className='m-2'>
+                    <p className='text-gray-400 m-1 leading-5'>{info}</p>
                 </div>
-                <div>
+                <div className='m-1'>
                     <p className='text-xl font-semibold'>{mobile}</p>
                 </div>
                 <div>
-                    <p className='text-gray-400'>{action}</p>
+                    <p className='text-gray-400 '>{action}</p>
                 </div>
-                <div >
+                <div className='mt-2' >
                     <Button name={btntext} className='w-1/2 text-sm rounded-md'/>
                 </div>
             </div>
--- client/src/Views/ContactUs.jsx
+++ client/src/Views/ContactUs.jsx
@@ -11,15 +11,15 @@ function ContactUs() {
             <Navbar />
             <div className='flex-1 grid place-items-center'>
                 <div className='flex justify-center items-center border rounded-lg m-5 mt-24 bg-[#FFFFFF] w-5/6 h-4/6 sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 2xl:w-3/5  '>
-                    <div className=''>
-                        <div className=''>
+                    <div className=' h-full p-4 mx-5  '>
+                        <div className=' my-2'>
                             <p className='text-3xl font-bold'>Get in Touch</p>
                         </div>
                         <div className=''>
                             <p className='font-light'>Need help? Have questions? We're here to support you 24/7. Reach out to us anytime - every inquiry matters.</p>
                         </div>
 
-                        <div className='flex'>
+                        <div className='flex my-2  h-max'>
                             <ContactUSCard title="Phone Support" info="Speak directly with our support team." mobile="+91 1234567890" action="24/7 Available" btntext="Call Now"/>
                             <ContactUSCard title="Email Support" info="Send us detailed queries." mobile="support@blooddonor.in" action="Response in 2 hours" btntext="Send Email"/>
                             <ContactUSCard title="WhatsApp" info="Quick support via WhatsApp." mobile="+91 9970714597" action="Business hours" btntext="Message"/>
