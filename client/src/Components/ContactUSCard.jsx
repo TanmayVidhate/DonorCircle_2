445d3a892660db90926c40eb32c721f443d875eb
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-20 11:06:22 +0530

    add transitions

--- client/src/Components/ContactUSCard.jsx
+++ client/src/Components/ContactUSCard.jsx
@@ -5,7 +5,7 @@ function ContactUSCard({className,Icon,title,info,mobile,action,btntext}) {
     console.log("mm=",Icon)
     return (
         <>
-            <div className='border rounded-md text-center w-2/6 h-[300px] m-2 p-2 hover:mt-0 '>
+            <div className='border rounded-md text-center w-2/6 h-[300px] m-2 p-2 hover:mt-0 cursor-pointer transition-all duration-500 ease-in-out'>
                 <div className={`m-auto  rounded-full w-[30%] h-[25%] mt-3 relative ${className}`}>
                     <p className='absolute right-6 top-6 text-white '><Icon/></p>
                 </div>
--- client/src/Views/ContactUs.jsx
+++ client/src/Views/ContactUs.jsx
@@ -15,7 +15,7 @@ function ContactUs() {
             <Navbar />
             <div className='flex-1 grid place-items-center'>
                 <div className='flex justify-center items-center border rounded-lg m-5 mt-24 bg-[#FFFFFF] w-5/6 h-4/6 sm:w-3/4 sm:px-5 lg:mt-10 lg:w-4/5 2xl:w-3/5  '>
-                    <div className=' h-full p-4 mx-5  '>
+                    <div className=' h-full p-4 mx-2  '>
                         <div className=' my-2'>
                             <p className='text-3xl font-bold'>Get in Touch</p>
                         </div>
@@ -23,8 +23,8 @@ function ContactUs() {
                             <p className='font-light'>Need help? Have questions? We're here to support you 24/7. Reach out to us anytime - every inquiry matters.</p>
                         </div>
 
-                        <div className='flex  my-2  h-max'>
-                            <ContactUSCard className="bg-cyan-300" Icon={Phone} title="Phone Support" info="Speak directly with our support team." mobile="+91 1234567890" action="24/7 Available" btntext="Call Now" />
+                        <div className='flex my-2  h-max  '>
+                            <ContactUSCard className="bg-cyan-300" Icon={Phone} title="Phone Support" info="Speak directly with our support team." mobile="+91 1234567890" action="24/7 Available" btntext="Call Now"  />
                             <ContactUSCard className="bg-red-300" Icon={Mail} title="Email Support" info="Send us detailed queries." mobile="support@blooddonor.in" action="Response in 2 hours" btntext="Send Email" />
                             <ContactUSCard className="bg-green-300" Icon={MessageSquare} title="WhatsApp" info="Quick support via WhatsApp." mobile="+91 9970714597" action="Business hours" btntext="Message" />
                         </div>
