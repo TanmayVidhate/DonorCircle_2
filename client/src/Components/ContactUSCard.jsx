cdfe857eb2996f5d3cb756eb585d4d1787a5dc49
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-21 12:11:02 +0530

    add md breakpoint

--- client/src/Components/ContactUSCard.jsx
+++ client/src/Components/ContactUSCard.jsx
@@ -5,10 +5,9 @@ function ContactUSCard({className,Icon,title,info,mobile,action,btntext,btnbgcol
 
     return (
         <>
-            
-            <div className='border border-red-700 rounded-md text-center w-5/6 h-[300px] m-5 p-2 '>
+            <div className='border border-red-700 rounded-md text-center w-5/6 h-[300px] m-5 p-2  md:w-[300px]'>
                 <div className={`m-auto  rounded-full w-[20%] h-[20%] mt-3 relative ${className} sm:w-[20%] sm:h-[20%]`}>
-                    <p className='absolute left-4 top-4 text-white sm:left-5 sm:top-4 '><Icon/></p>
+                    <p className='absolute left-4 top-4 text-white sm:left-5 sm:top-4 md:left-4'><Icon/></p>
                 </div>
                 <div className='mt-3'>
                     <p>{title}</p>
