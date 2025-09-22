def7a05a2ec11fc2ebfef7c97da7a2b3986cd46c
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-22 23:22:39 +0530

    add margin

--- client/src/Views/ContactUs.jsx
+++ client/src/Views/ContactUs.jsx
@@ -14,7 +14,7 @@ function ContactUs() {
         <div className='flex flex-col w-screen min-h-screen bg-[#ffd6a5] xl:h-screen '>
             <Navbar />
             <div className='flex justify-center items-center'>
-                <div className='flex justify-center items-center rounded-lg  bg-[#FFFFFF]  mt-28  mx-10 my-10 sm:min-h-max sm:w-3/4 sm:px-5 lg:mt-24 lg:w-4/5 2xl:w-3/5 '>
+                <div className='flex justify-center items-center rounded-lg  bg-[#FFFFFF]  mt-28  mx-10 my-10 sm:min-h-max sm:w-3/4 sm:px-5 lg:mt-36 lg:w-4/5 2xl:w-3/5 '>
                     <div className='w-full h-full p-4 '>
                         <div className=''>
                             <p className='text-3xl font-bold '>Get in Touch</p>
