1a3bc197f50bad26e4a7959358acb6a50cf11770
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-25 00:02:12 +0530

    add class

--- client/src/Views/ContactUs.jsx
+++ client/src/Views/ContactUs.jsx
@@ -12,7 +12,7 @@ import { Phone, Mail, MessageSquare } from 'lucide-react';
 
 function ContactUs() {
     return (
-        <div className='flex flex-col w-screen min-h-screen bg-[#ffd6a5] xl:h-screen '>
+        <div className='flex flex-col w-screen min-h-min bg-[#ffd6a5] xl:h-screen  '>
             <Navbar />
             <div className='flex justify-center items-center'>
                 <div className='flex justify-center items-center rounded-lg  bg-[#FFFFFF]  mt-28  mx-10 my-10 sm:min-h-max sm:w-3/4 sm:px-5 lg:mt-36 lg:w-4/5 2xl:w-3/5 '>
@@ -26,13 +26,13 @@ function ContactUs() {
 
                         <div className='flex my-2 flex-col w-full h-3/5 items-center lg:flex-row  lg:justify-center  '>
                             <ContactUSCard className="bg-cyan-300" Icon={Phone} title="Phone Support" info="Speak directly with our support team." mobile="+91 1234567890" action="24/7 Available" btntext="Call Now" btnbgcolor="bg-cyan-300" />
-                            <ContactUSCard className="bg-red-300" Icon={Mail} title="Email Support" info="Send us detailed queries." mobile="support@blooddonor.in" action="Response in 2 hours" btntext="Send Email" btnbgcolor="bg-red-300"/>
-                            <ContactUSCard className="bg-green-300" Icon={MessageSquare} title="WhatsApp" info="Quick support via WhatsApp." mobile="+91 9970714597" action="Business hours" btntext="Message" btnbgcolor="bg-green-300"/>
+                            <ContactUSCard className="bg-red-300" Icon={Mail} title="Email Support" info="Send us detailed queries." mobile="support@blooddonor.in" action="Response in 2 hours" btntext="Send Email" btnbgcolor="bg-red-300" />
+                            <ContactUSCard className="bg-green-300" Icon={MessageSquare} title="WhatsApp" info="Quick support via WhatsApp." mobile="+91 9970714597" action="Business hours" btntext="Message" btnbgcolor="bg-green-300" />
                         </div>
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
