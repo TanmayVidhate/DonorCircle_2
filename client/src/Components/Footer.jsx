f4873533f0ff1f2f0c4c9e2c505f17acdc09f692
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-24 23:59:13 +0530

    add footer component

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
+import LinkTo from './LinkTo'
 
 function Footer() {
     return (
         <>
-            <div className='flex justify-center w-full border border-red-500 gap-80'>
-                <div className='border border-red-500'>
-                    <p>First Div</p>
+            <div className='flex justify-center w-full border border-red-500 '>
+                <div className='border border-red-500 w-full text-center'>
+                    <div>DonorCircle.in</div>
+                    <div className='text-left font-semibold'>Connecting blood donors with those in need. Together, we save lives.</div>
                 </div>
 
-                <div>
-                    <p>Quick Links</p>
+                <div className='w-full border border-red-500 text-center'> 
+                    <div className='text-center'>Quick Links</div>
+                    <LinkTo PageName="About us" url="/aboutus" className="text-black capitalize" />
+                    <LinkTo PageName="Home" url="/" className="text-black capitalize"/>
                 </div>
 
-                <div>
+                <div className='w-full border border-red-500 text-center'>
                     <p>Blood Groups</p>
                 </div>
 
-                <div>
+                <div className='w-full border border-red-500 text-center'>
                     <p>Support</p>
+
                 </div>
             </div>
         </>
