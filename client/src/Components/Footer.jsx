719010ddaa08be607e823b70578bac42ad9f8844
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-27 11:37:53 +0530

    remove border

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -6,18 +6,18 @@ function Footer() {
         <>
             <div className='bg-[#243447]  h-2/4 '>
                 <div className='flex justify-center mt-5 sm:flex-col md:flex-row'>
-                    <div className='  w-1/4 p-2 sm:w-3/4 sm:mx-8 md:w-10/12 border border-yellow-500'>
+                    <div className='  w-1/4 p-2 sm:w-3/4 sm:mx-8 md:w-10/12 '>
                         <div className='font-bold text-red-500 text-lg sm:text-left '>DonorCircle.in</div>
                         <div className='text-left font-semibold text-white   '>Connecting donors and recipients to save lives — every match brings hope and healing.</div>
                     </div>
 
-                    <div className=' w-1/4 p-2  sm:mx-8 md:w-1/3 border border-yellow-500'>
+                    <div className=' w-1/4 p-2  sm:mx-8 md:w-1/3  '>
                         <div className=' font-bold text-red-500 text-lg sm:text-left'>Quick Links</div>
                         <LinkTo PageName="About us" url="/aboutus" className="text-white capitalize sm:text-left" />
                         <LinkTo PageName="Home" url="/" className="text-white capitalize sm:text-left" />
                     </div>
 
-                    <div className=' w-1/4 p-2 sm:mx-8 border border-yellow-500 sm:text-left md:w-1/3'>
+                    <div className=' w-1/4 p-2 sm:mx-8  sm:text-left md:w-1/3'>
                         <div className='font-bold text-red-500 text-lg'>Blood Groups</div>
                         <div className='text-white'>A+</div>
                         <div className='text-white'>B+</div>
