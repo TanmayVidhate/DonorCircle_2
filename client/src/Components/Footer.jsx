ad2ce420aa19ad1a022a4234017d233973ddadc2
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-27 12:51:06 +0530

    add margin

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -6,18 +6,18 @@ function Footer() {
         <>
             <div className='bg-[#243447]  h-3/6  lg:p-8'>
                 <div className='flex-row justify-center mt-5 sm:flex-col md:flex-row lg:flex'>
-                    <div className=' w-2/3 mx-10 sm:w-3/4 sm:mx-8 md:w-10/12'>
+                    <div className=' w-2/3 mx-7 sm:w-3/4 sm:mx-8 md:w-10/12'>
                         <div className='font-bold text-red-500 text-lg sm:text-left '>DonorCircle.in</div>
                         <div className='text-left font-semibold text-white   '>Connecting donors and recipients to save lives — every match brings hope and healing.</div>
                     </div>
 
-                    <div className=' w-2/3 mx-10 my-2 sm:mx-8 md:w-1/3  '>
+                    <div className=' w-2/3 mx-7 my-2 sm:mx-8 md:w-1/3  '>
                         <div className='  font-bold text-red-500 text-lg sm:text-left'>Quick Links</div>
                         <LinkTo PageName="About us" url="/aboutus" className="!text-left text-white capitalize sm:text-left " />
                         <LinkTo PageName="Home" url="/" className="!text-left text-white capitalize sm:text-left" />
                     </div>
 
-                    <div className=' w-2/4 mx-10 sm:mx-8  sm:text-left md:w-1/3'>
+                    <div className=' w-2/4 mx-7 sm:mx-8  sm:text-left md:w-1/3'>
                         <div className='font-bold text-red-500 text-lg '>Blood Groups</div>
                         <div className='text-white'>A+</div>
                         <div className='text-white'>B+</div>
@@ -25,7 +25,7 @@ function Footer() {
                         <div className='text-white'>AB+</div>
                     </div>
 
-                    <div className=' w-1/4 mx-10 sm:mx-8 sm:text-left'>
+                    <div className=' w-1/4 mx-7 sm:mx-8 sm:text-left'>
                         <div className='font-semibold text-red-500 text-lg'>Support</div>
                     </div>
 
