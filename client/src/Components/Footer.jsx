91e6e7ef18107a7883751d2e9bb236360045687e
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-09-27 11:29:58 +0530

    add md breakpoint

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -5,19 +5,19 @@ function Footer() {
     return (
         <>
             <div className='bg-[#243447]  h-2/4 '>
-                <div className='flex justify-center mt-5 sm:flex-col'>
-                    <div className=' text-center w-1/4 p-2 sm:w-3/4 sm:mx-8 md:w-10/12 border border-yellow-500'>
+                <div className='flex justify-center mt-5 sm:flex-col md:flex-row'>
+                    <div className='  w-1/4 p-2 sm:w-3/4 sm:mx-8 md:w-10/12 border border-yellow-500'>
                         <div className='font-bold text-red-500 text-lg sm:text-left '>DonorCircle.in</div>
                         <div className='text-left font-semibold text-white   '>Connecting donors and recipients to save lives — every match brings hope and healing.</div>
                     </div>
 
-                    <div className='text-center w-1/4 p-2  sm:mx-8  border border-yellow-500'>
-                        <div className='text-center font-bold text-red-500 text-lg sm:text-left'>Quick Links</div>
+                    <div className=' w-1/4 p-2  sm:mx-8 md:w-1/3 border border-yellow-500'>
+                        <div className=' font-bold text-red-500 text-lg sm:text-left'>Quick Links</div>
                         <LinkTo PageName="About us" url="/aboutus" className="text-white capitalize sm:text-left" />
                         <LinkTo PageName="Home" url="/" className="text-white capitalize sm:text-left" />
                     </div>
 
-                    <div className='text-center w-1/4 p-2 sm:mx-8 border border-yellow-500 sm:text-left'>
+                    <div className=' w-1/4 p-2 sm:mx-8 border border-yellow-500 sm:text-left md:w-1/3'>
                         <div className='font-bold text-red-500 text-lg'>Blood Groups</div>
                         <div className='text-white'>A+</div>
                         <div className='text-white'>B+</div>
@@ -25,7 +25,7 @@ function Footer() {
                         <div className='text-white'>AB+</div>
                     </div>
 
-                    <div className='text-center w-1/4 p-2 sm:mx-8 sm:text-left'>
+                    <div className=' w-1/4 p-2 sm:mx-8 sm:text-left'>
                         <div className='font-semibold text-red-500 text-lg'>Support</div>
                     </div>
 
