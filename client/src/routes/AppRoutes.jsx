52cd1328bcb09cc2e9bc50ec9361f22315b240cb
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-12-18 19:04:05 +0530

    change code

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -19,62 +19,19 @@ const LandingPage = lazy(()=> import("../Views/LandingPage.jsx"))
 
 function AppRoutes() {
     return (
-        <Routes>
-            <Route path="/" element={
-                <Suspense fallback={<Loader/>}>
-                    <LandingPage />
-                </Suspense>
-            } />
-            
-            <Route path="/signup" element={
-                <Suspense fallback={<Loader/>}>
-                    <SignupForm />
-                </Suspense>
-            } />
-
-            <Route path="/signin" element={
-                <Suspense fallback={<Loader/>}>
-                    <Signin />
-                </Suspense>
-                
-            } />
-            <Route path="/showalluers" element={
-                <Suspense fallback={<Loader/>}>
-                    <ShowAllUser />
-                </Suspense>
-            } />
-
-            <Route path="/showuserinfo/:email" element={
-                <Suspense fallback={<Loader/>}>
-                    <ShowAllUserinfo />
-                </Suspense>
-            } />
-
-            <Route path="/adduserprofile" element={
-                <Suspense fallback={<Loader/>}>
-                    <AddUserProfile />
-                </Suspense>
-            } />
-
-            <Route path='/aboutus' element={
-                <Suspense fallback={<Loader/>}>
-                    <AboutUs />
-                </Suspense>
-            } />
-
-            <Route path='/contactus' element={
-                <Suspense fallback={<Loader/>}>
-                    <ContactUs />
-                </Suspense>
-            } />
-
-            <Route path='/faq' element={
-                <Suspense fallback={<Loader/>}>
-                    <FAQ />
-                </Suspense>
-            } />
-            
-        </Routes>
+        <Suspense fallback={<Loader />}>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/signup" element={<SignupForm />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/showalluers" element={<ShowAllUser />} />
+                <Route path="/showuserinfo/:email" element={<ShowAllUserinfo />} />
+                <Route path="/adduserprofile" element={<AddUserProfile />} />
+                <Route path='/aboutus' element={<AboutUs />} />
+                <Route path='/contactus' element={<ContactUs />} />
+                <Route path='/faq' element={<FAQ />} />
+            </Routes>
+        </Suspense>
     )
 }
 
