d7f4f5db3b685d0008ecf1cf655bf0aacea89104
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-05 16:45:13 +0530

    import component and context

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 
+import {UserContext} from "../Context/UserContext.jsx";
+
 //import components
 import Button from "./Button";
+import FilterUsers from "./FilterUsers"
 
 //Translation
 import { useTranslation } from 'react-i18next';
 
-function SearchBar({ searchval, setSearchval, filterRecord}) {
+function SearchBar({ searchval, setSearchval, filterRecord }) {
+
+  const { role, setRole } = useContext(UserContext);
 
   const { t, i18n } = useTranslation();
 
@@ -24,20 +29,22 @@ function SearchBar({ searchval, setSearchval, filterRecord}) {
               setSearchval(e.target.value);
             }}
             placeholder={t(`${"search_blood"}`)}
-            className="flex-1 px-4 py-2 border  rounded-lg
+            className="flex-1 px-4 py-2 border  rounded-lg 
                        text-gray-700 w-3/6 text-sm sm:text-base
                        focus:outline-none focus:ring-0 focus:bg-transparen  "
           />
-
+          <div className=" z-50" >
+            <FilterUsers role={role} setRole={setRole} />
+          </div>
           <Button
             name={t(`${"search"}`)}
             onClick={filterRecord}
-            disabled={searchval === "" && true  }
+            disabled={searchval === "" && true}
             className="!w-1/3  py-2 bg-[#ffd6a5] text-[#c2410c] rounded-lg 
                        text-sm sm:text-base font-semibold
                        hover:bg-[#f7c789] transition-all duration-200"
           >
-            
+
           </Button>
 
         </div>
