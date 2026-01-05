c1cdff9c24d06e5c75d016e0aab806ae31c6184b
tanmay-vidhate <tanmayvidhate7@gmail.com>
2026-01-05 18:47:50 +0530

    import context and add tailwindcss

--- client/src/Components/FilterUsers.jsx
+++ client/src/Components/FilterUsers.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 
-import {UserContext} from "../Context/UserContext.jsx";
+import { UserContext } from "../Context/UserContext.jsx";
 
 //Lucide import
 import { FunnelPlus } from 'lucide-react';
@@ -9,25 +9,27 @@ function FilterUsers() {
     const { role, setRole } = useContext(UserContext);
 
     const [isOpen, setIsOpen] = useState(false);
-    
-    
+
+
     return (
         <>
-            <div className='relative w-full '>
-                <FunnelPlus className='hover:text-[#ffd6a5] cursor-pointer ' onClick={() => { setIsOpen(!isOpen) }} />
+            <div className=' w-full  '>
+                <FunnelPlus className='hover:text-[#ffd6a5] cursor-pointer  ' onClick={() => { setIsOpen(!isOpen) }} />
                 {(isOpen)
                     &&
-                    <div className=' border border-red-600 absolute'>
-                        <label>
-                            <input type="radio" name="role" value="donor" checked={role === "donor"} onChange={(e) => setRole(e.target.value)} />
-                            Donor
-                        </label>
-
-                        <label>
-                            <input type="radio" name="role" value="receiver" checked={role === "receiver"} onChange={(e) => setRole(e.target.value)} />
-                            Receiver
-                        </label>
-
+                    <div className='absolute mt-2 px-2 py-2 rounded cursor-pointer bg-[#f7c789] text-white'>
+                        <div>
+                            <label  >
+                                <input className='m-1' type="radio" name="role" value="donor" checked={role === "donor"} onChange={(e) => setRole(e.target.value)} />
+                                    Donors
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input className='m-1' type="radio" name="role" value="receiver" checked={role === "receiver"} onChange={(e) => setRole(e.target.value)} />
+                                    Receivers
+                            </label>
+                        </div>
                     </div>
                 }
             </div>
